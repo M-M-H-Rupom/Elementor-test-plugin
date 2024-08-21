9a7f1a21228af6cf101f3793b9ff3e8ebf813622
Rupom <mohrajulrupom@gmail.com>
2024-08-21 23:52:35 +0600

    dynamic pricing table

--- widgets/pricing-table.php
+++ widgets/pricing-table.php
@@ -32,8 +32,8 @@ class Elementor_pricing_Widget extends \Elementor\Widget_Base {
 			[
 				'label' => esc_html__( 'Title', 'elementor_test' ),
 				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'Default title', 'elementor_test' ),
-				'placeholder' => esc_html__( 'Type your title here', 'elementor_test' ),
+				'default' => esc_html__( 'Pricing', 'elementor_test' ),
+				'placeholder' => esc_html__( 'Title', 'elementor_test' ),
 			]
 		);
 
@@ -44,6 +44,15 @@ class Elementor_pricing_Widget extends \Elementor\Widget_Base {
 				'type' => \Elementor\Controls_Manager::REPEATER,
 				'fields' => [
 					[
+						'name' => 'pricing_featured',
+						'label' => esc_html__( 'Featured', 'elementor_test' ),
+						'type' => \Elementor\Controls_Manager::SWITCHER,
+						'label_on' => esc_html__( 'Yes', 'elementor_test' ),
+						'label_off' => esc_html__( 'No', 'elementor_test' ),
+						'return_value' => 'yes',
+						'default' => false,
+					],
+					[
 						'name' => 'pricing_title',
 						'label' => esc_html__( 'Title', 'elementor_test' ),
 						'type' => \Elementor\Controls_Manager::TEXT,
@@ -53,9 +62,19 @@ class Elementor_pricing_Widget extends \Elementor\Widget_Base {
 					[
 						'name' => 'pricing_description',
 						'label' => esc_html__( 'Description', 'elementor_test' ),
-						'type' => \Elementor\Controls_Manager::WYSIWYG,
+						'type' => \Elementor\Controls_Manager::TEXTAREA,
 						'default' => esc_html__( 'Description' , 'elementor_test' ),
 						'show_label' => false,
+					],
+					[
+						'name' => 'item_price',
+						'label' => esc_html__( 'Price', 'elementor_test' ),
+						'type' => \Elementor\Controls_Manager::NUMBER,
+						// 'label_block' => true,
+						// 'min' => 0,
+						// 'max' => 100,
+						// 'step' => 5,
+						'default' => 99,
 					],
 					[
 						'name' => 'pricing_button',
@@ -68,24 +87,33 @@ class Elementor_pricing_Widget extends \Elementor\Widget_Base {
 						'name' => 'pricing_button_url',
 						'label' => esc_html__( 'Url', 'elementor_test' ),
 						'type' => \Elementor\Controls_Manager::URL,
-						'default' => esc_html__( 'Url' , 'elementor_test' ),
+						'default' => [
+							'url' => '#',
+							'is_external' => false,
+							'nofollow' => false,
+						],
 						'label_block' => true,
 					],
-					
 				],
-				// 'default' => [
-				// 	[
-				// 		'pricing_title' => esc_html__( 'Title', 'elementor_test' ),
-				// 		'pricing_description' => esc_html__( 'Item content', 'elementor_test' ),
-				// 	],
-					
-				// ],
-				'title_field' => '{{{ pricing_title  }}}',
+				'default' => [
+					[
+						'pricing_title' => esc_html__( 'Basic Plan', 'elementor_test' ),
+						'pricing_description' => esc_html__( 'Far far away, behind the word mountains, far from the countries Vokalia .for begginers', 'elementor_test' ),
+					],
+					[
+						'pricing_title' => esc_html__( 'Advanced Plan', 'elementor_test' ),
+						'pricing_description' => esc_html__( 'Separated they live in Bookmarksgrove right at the coast, a large language ocean.', 'elementor_test' ),
+					],
+					[
+						'pricing_title' => esc_html__( 'Pro Plan', 'elementor_test' ),
+						'pricing_description' => esc_html__( 'lead">Even the all-powerful Pointing has no control about the blind texts it is an almost life.', 'elementor_test' ),
+					],
+				],
+				'title_field' => '{{{ pricing_title }}}',
 			]
 		);
 
 		$this->end_controls_section();
-
 	}
 
 	protected function render() {
@@ -95,53 +123,36 @@ class Elementor_pricing_Widget extends \Elementor\Widget_Base {
 			<div class="container">
 				<div class="row text-center">
 					<div class="col">
-						<h1 class="text-white">Pricing</h1>
+						<h1 class="text-white"><?php echo esc_html($settings['price_title']); ?></h1>
 					</div>
 				</div>
 
 				<div class="row mt-5 align-items-center">
-					<div class="col-12 col-sm-10 col-md-8 m-auto col-lg-4 text-center">
-						<div class="fdb-box p-4">
-							<h2>Hobby</h2>
-							<p class="lead">Far far away, behind the word mountains, far from the countries Vokalia.</p>
-
-							<p class="h1 mt-5 mb-5">$99</p>
-
-							<p><a href="https://www.froala.com" class="btn btn-dark">Buy Now</a></p>
+					<?php foreach ($settings['pricing_column'] as $pricing_item) {
+						$button_color_class = $pricing_item['pricing_featured']? 'primary' : 'dark';
+						$featured_pricing_padding = $pricing_item['pricing_featured']? 'px-4 py-5' : 'p-4';
+						?>
+						<div class="col-12 col-sm-10 col-md-8 m-auto col-lg-4 text-center">
+							<div class="fdb-box <?php echo esc_attr($featured_pricing_padding) ?> ">
+								<h2><?php echo esc_html($pricing_item['pricing_title']); ?></h2>
+								<p class="lead"><?php echo esc_html($pricing_item['pricing_description']); ?></p>
+								<p class="h1 mt-5 mb-5">$<?php echo esc_html($pricing_item['item_price']); ?></p>
+								<p>
+									<a href="<?php echo esc_url($pricing_item['pricing_button_url']['url']); ?>" class="btn btn-<?php echo esc_attr($button_color_class) ?>">
+										<?php echo esc_html($pricing_item['pricing_button']); ?>
+									</a>
+								</p>
+							</div>
 						</div>
-					</div>
-
-					<div class="col-12 col-sm-10 col-md-8 m-auto col-lg-4 text-center pt-4 pt-lg-0">
-						<div class="fdb-box px-4 pt-5">
-							<h2>Advanced</h2>
-							<p class="lead">Separated they live in Bookmarksgrove right at the coast, a large language ocean.</p>
-
-							<p class="h1 mt-5 mb-5">$299</p>
-
-							<p><a href="https://www.froala.com" class="btn btn-secondary">Buy Now</a></p>
-						</div>
-					</div>
-
-					<div class="col-12 col-sm-10 col-md-8 m-auto col-lg-4 text-center pt-4 pt-lg-0">
-						<div class="fdb-box p-4">
-							<h2>Business</h2>
-							<p class="lead">Even the all-powerful Pointing has no control about the blind texts it is an almost life.</p>
-
-							<p class="h1 mb-5 mt-5">$799</p>
-
-							<p><a href="https://www.froala.com" class="btn btn-dark">Buy Now</a></p>
-						</div>
-					</div>
+					<?php }; ?>
 				</div>
 			</div>
 		</section>
+
 		<?php
-		
 	}
 
 	protected function content_template() {
-		
+		// Leave empty or implement JS-based dynamic rendering if needed
 	}
-
-	
 }
